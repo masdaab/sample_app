cf98a65ee4d6c7454dc8b93e938fabe9749f9bfe
zinux <zinux@zinux-0S.(none)>
2013-11-09 07:16:41 +0700

    end static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do
 
   
+  get "static_pages/about"
   get "test_page/init"
   get "test_page/edit"
   get "static_pages/home"
   get "static_pages/help"
+
+  get "static_pages/test"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
