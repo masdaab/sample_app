c0e4bc93c0a066e99355c0c0e3ddf2430427eccc
zinux <zinux@zinux-0S.(none)>
2013-11-09 08:32:37 +0700

    end static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Home')
     end
 
     it "should have the title 'Home'" do
@@ -32,7 +32,7 @@ describe "Static pages" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About')
+      expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
